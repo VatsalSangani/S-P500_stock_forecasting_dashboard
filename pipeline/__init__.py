012c969deeeaac922987b79deddd415aa2142727
Vatsal_Sangani <77208467+VatsalSangani@users.noreply.github.com>
2025-09-30 18:04:55 +0100

    Update __init__.py

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,13 +1,14 @@
 # pipeline/__init__.py
 
-from .extract import run_extraction
-from .transform import run_transformation
-from .forecast import run_forecasts
+# from .extract import run_extraction
+# from .transform import run_transformation
+# from .forecast import run_forecasts
 from .config_sp500 import SP500_TICKERS
 
 __all__ = [
-    "run_extraction",
-    "run_transformation",
-    "run_forecasts",
     "SP500_TICKERS",
 ]
+
+# "run_extraction", "run_transformation","run_forecasts" add this above when you are actually trying to streamline and compute everything
+    
+    
